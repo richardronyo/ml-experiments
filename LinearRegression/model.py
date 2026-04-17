bbe7446139561fd0fdeb2c1bc0c52611e1060880
richardronyo <62360494+richardronyo@users.noreply.github.com>
2026-04-16 19:29:30 -0600

    Completed ISLP Chapter 2 Review and Exercises in PreLim folder

--- LinearRegression/model.py
+++ LinearRegression/model.py
@@ -1,9 +1,21 @@
 import numpy as np
 
-
 class LinearRegression:
     def __init__(self):
         theta = np.array([])
         x = np.array([1])
     
+    def fit(self, X, Y):
+        p = X.shape[1]
+        new_x = np.zeros(p + 1)
+        new_x[0] = 1
+        
+        self.x = new_x
+        self.y_true = Y
+        self.theta = np.zeros(p + 1) 
 
+
+        return
+
+    def gradient_descent(self):
+        return
